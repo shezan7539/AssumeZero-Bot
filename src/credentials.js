8f1a3a8c024c7b5c458dc18ff57c9dcb065bab2a
shezan7539 <75947263+shezan7539@users.noreply.github.com>
2022-08-25 12:45:21 +0600

    Create credentials.js

---
+++ src/credentials.js
@@ -0,0 +1,31 @@
+// Facebook log-in credentials for bot account
+
+exports.FACEBOOK_EMAIL = "mdmizanurrahman.tct@gmail.com";
+
+exports.FACEBOOK_PASSWORD = "M!Z@NUR7539";
+
+// Heroku application token
+
+exports.TOKEN = "";
+
+// MemCachier credentials (from MemCachier dashboard)
+
+exports.MEMCACHIER_PASSWORD = "92B5FA6403A86EC7E7EAE123B32E6E3F";
+
+exports.MEMCACHIER_SERVERS = "mc3.dev.ec2.memcachier.com:11211";
+
+exports.MEMCACHIER_USERNAME = "24CF71";
+
+// Spotify API credentials
+
+exports.SPOTIFY_CLIENTID = "";
+
+exports.SPOTIFY_CLIENTSECRET = "";
+
+// Wolfram short-answer API key
+
+exports.WOLFRAM_KEY = "";
+
+// Open Weather Map API key
+
+exports.WEATHER_KEY = "";
